3b9b66bf62f96aedc34fec584e5967a6f2570d5e
Georgije Bosiger <georgije.bosiger@elektrobit.com>
2024-12-02 10:33:01 +0100

    adding git and small changes

---
+++ lua/plugins/git.lua
@@ -0,0 +1,16 @@
+return {
+	{
+		"lewis6991/gitsigns.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("gitsigns").setup()
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+      require("diffview").setup()
+    end,
+	},
+}
--- lua/plugins/rustacean.lua
+++ lua/plugins/rustacean.lua
@@ -1,39 +1,64 @@
 return {
-  "mrcjkb/rustaceanvim",
-  version = "^4",
-  lazy = false,
-  ["rust-analyzer"] = {
-    cargo = {
-      allFeatures = true,
-    },
-  },
-  config = function()
-    local mason_registry = require("mason-registry")
+	{
+		"rust-lang/rust.vim",
+		ft = "rust",
+		init = function()
+			vim.g.rustfmt_autosave = 1
+		end,
+	},
+	{
+		"saecki/crates.nvim",
+		ft = { "toml" },
+		config = function()
+			require("crates").setup({
+				completion = {
+					cmp = {
+						enabled = true,
+					},
+				},
+			})
+			require("cmp").setup.buffer({
+				sources = { { name = "crates" } },
+			})
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		version = "^5",
+		lazy = false,
+		["rust-analyzer"] = {
+			cargo = {
+				allFeatures = true,
+			},
+		},
+		config = function()
+			local mason_registry = require("mason-registry")
 
-    local codelldb = mason_registry.get_package("codelldb")
-    local extension_path = codelldb:get_install_path() .. "/extension/"
-    local codelldb_path = extension_path .. "adapter/codelldb"
-    local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
+			local codelldb = mason_registry.get_package("codelldb")
+			local extension_path = codelldb:get_install_path() .. "/extension/"
+			local codelldb_path = extension_path .. "adapter/codelldb"
+			local liblldb_path = extension_path .. "lldb/lib/liblldb.so"
 
-    local bufnr = vim.api.nvim_get_current_buf()
-    vim.keymap.set("n", "<leader>a", function()
-      vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
-      -- or vim.lsp.buf.codeAction() if you don't want grouping.
-    end, { silent = true, buffer = bufnr })
-    vim.keymap.set(
-      "n",
-      "K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
-      function()
-        vim.cmd.RustLsp({ "hover", "actions" })
-      end,
-      { silent = true, buffer = bufnr }
-    )
+			local bufnr = vim.api.nvim_get_current_buf()
+			vim.keymap.set("n", "<leader>a", function()
+				vim.cmd.RustLsp("codeAction") -- supports rust-analyzer's grouping
+				-- or vim.lsp.buf.codeAction() if you don't want grouping.
+			end, { silent = true, buffer = bufnr })
+			vim.keymap.set(
+				"n",
+				"K", -- Override Neovim's built-in hover keymap with rustaceanvim's hover actions
+				function()
+					vim.cmd.RustLsp({ "hover", "actions" })
+				end,
+				{ silent = true, buffer = bufnr }
+			)
 
-    local cfg = require("rustaceanvim.config")
-    return {
-      dap = {
-        adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
-      },
-    }
-  end,
+			local cfg = require("rustaceanvim.config")
+			return {
+				dap = {
+					adapter = cfg.get_codelldb_adapter(codelldb_path, liblldb_path),
+				},
+			}
+		end,
+	},
 }
