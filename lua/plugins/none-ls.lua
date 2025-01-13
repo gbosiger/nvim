03b8859e1c12874c27a35e66beece6fb671a849d
Georgije Bosiger <georgije.bosiger@elektrobit.com>
2025-01-13 23:27:00 +0100

    switching to conform from none-ls due to error messages and I guess more relevant approach. none-ls still present as disabled for now

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,10 +1,12 @@
 return {
   "nvimtools/none-ls.nvim",
+  enabled = false,
   lazy = false,
   config = function()
     local null_ls = require("null-ls")
 
     null_ls.setup({
+
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.black,
