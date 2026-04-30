400c34e2192e50302cb1b42e33a2e2b899d33d31
Georgije Bosiger <georgije.bosiger@elektrobit.com>
2026-04-30 22:46:55 +0200

    hot plug theme change

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,9 +1,30 @@
 return {
 	"catppuccin/nvim",
 	config = function()
+		local function catppuccin_scheme_from_mode()
+			local mode_file = vim.fn.expand("~/.config/catppuccin-mode")
+			local fd = io.open(mode_file, "r")
+			if not fd then
+				return "catppuccin-frappe"
+			end
+
+			local mode = fd:read("*l")
+			fd:close()
+
+			if mode == "latte" then
+				return "catppuccin-latte"
+			end
+
+			if mode == "frappe" then
+				return "catppuccin-frappe"
+			end
+
+			return "catppuccin-frappe"
+		end
+
 		require("catppuccin").setup({
 			auto_integrations = true,
 		})
-		vim.cmd.colorscheme("catppuccin-macchiato")
+		vim.cmd.colorscheme(catppuccin_scheme_from_mode())
 	end,
 }
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -3,9 +3,6 @@ return {
 	dependencies = { "nvim-tree/nvim-web-devicons" },
 	config = function()
 		require("lualine").setup({
-			options = {
-				theme = "catppuccin-macchiato",
-			},
 			extensions = { "neo-tree", "trouble" },
 		})
 	end,
